cc350d657916f189c151d8de580b91ad3dd328ee
alok-003 <alokchawat@gmail.com>
2024-08-21 20:34:30 +0530

    Add files via upload

---
+++ Reverse the PRN.cpp
@@ -0,0 +1,25 @@
+//Name:Alok Chawat
+//PRN:23070123016
+//Aim:Implemention of Decision Making Statements Loops to Reverse the PRN
+
+#include <iostream>
+using namespace std;
+
+int main() {
+     int PRN,reverse;
+    cout<<"Enter your PRN:"<<endl;
+    cin>>PRN;
+    while(PRN!=0){
+        reverse=PRN%10;
+        PRN=PRN/10;
+        cout<<reverse;
+    }
+        
+    return 0;
+}
+
+
+/*Output:
+Enter your PRN:
+23070
+07032*/
